bffbb38a0262b6e2d0713d84ae7d019f4cb5376a
H Parra <h@hgpa.tv>
2024-10-16 20:20:25 -0700

    I need info!

--- webpack-config.js
+++ webpack-config.js
@@ -1,74 +1,69 @@
-module.exports = {
-  // context: path.resolve(__dirname),
-  // devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        // includes, excludes are in tsconfig.json
-        test: /\.ts?$/,
-        exclude: /node_modules/,
-        use: 'ts-loader'
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Custom Webpack plugin to log build info that aio isn't giving us.
+ */
+class CustomStatsPlugin {
+  apply(compiler) {
+    compiler.hooks.done.tap('CustomStatsPlugin', (stats) => {
+      const log = (msg) => console.log(`\n\t${msg}`);
+
+      const mainEntry = stats.compilation.entrypoints.get('main');
+      if (!mainEntry) {
+        log('Main entry not found. Weird.');
+        return;
       }
-    ]
-  },
-  optimization: {
-    minimize: false
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.ts',
-    ],
-    mainFields: [
-      'main'
-    ],
-  },
-  target: 'node',
+      const outputPath = stats.compilation.outputOptions.path;
+      const chunks = mainEntry.chunks;
+      chunks.forEach(chunk => {
+        chunk.files.forEach(file => {
+          const fullPath = path.resolve(outputPath, file);
+          const relativePath = path.relative(process.cwd(), fullPath);
+          const fileSize = (fs.statSync(fullPath).size / 1024 / 1024).toFixed(2);
+          const duration = stats.endTime - stats.startTime;
+          log(`Built ${relativePath} (${fileSize} MB) in ${duration} ms`);
+        });
+      });
+    });
+  }
 }
 
-/*
-  @adobe/aio-lib-runtime:action-builder:debug merged webpack config : {
-  @adobe/aio-lib-runtime:action-builder:debug   "devtool": "inline-source-map",
-  @adobe/aio-lib-runtime:action-builder:debug   "module": {
-  "rules": [
-    {
-      "test": {},
-      "exclude": {},
-      "use": "ts-loader"
-    }
-  ]
-  @adobe/aio-lib-runtime:action-builder:debug   },
-  @adobe/aio-lib-runtime:action-builder:debug   "resolve": {
-  "extensions": [
-    ".ts",
-    ".js",
-    ".json"
-  ],
-  "mainFields": [
-    "main"
-  ]
-  @adobe/aio-lib-runtime:action-builder:debug   },
-  @adobe/aio-lib-runtime:action-builder:debug   "entry": [
-  "/Users/hgpa/hgpa/git/github.com/hparra/aio-app-ts-test/actions/generic/index.ts"
-  @adobe/aio-lib-runtime:action-builder:debug   ],
-  @adobe/aio-lib-runtime:action-builder:debug   "output": {
-  "libraryTarget": "commonjs2",
-  "path": "/Users/hgpa/hgpa/git/github.com/hparra/aio-app-ts-test/dist/application/actions/aio-app-ts-test/generic-temp",
-  "filename": "index.js"
-  @adobe/aio-lib-runtime:action-builder:debug   },
-  @adobe/aio-lib-runtime:action-builder:debug   "target": "node",
-  @adobe/aio-lib-runtime:action-builder:debug   "mode": "production",
-  @adobe/aio-lib-runtime:action-builder:debug   "optimization": {
-  "minimize": false
-  @adobe/aio-lib-runtime:action-builder:debug   },
-  @adobe/aio-lib-runtime:action-builder:debug   "plugins": [
-  {
-    "definitions": {
-      "WEBPACK_ACTION_BUILD": "true",
-      "process.env.AIO_CLI_ENV": "\"prod\""
-    }
+/**
+ * @param {object} env - the environment variables
+ * @param {object} argv - the command line arguments
+ * @see https://developer.adobe.com/app-builder/docs/guides/configuration/webpack-configuration/
+ * @see https://webpack.js.org/configuration/
+ */
+module.exports = (/*env, argv*/) => {
+  return {
+    // devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          // includes, excludes are in tsconfig.json
+          test: /\.ts?$/,
+          exclude: /node_modules/,
+          use: 'ts-loader'
+        }
+      ]
+    },
+    optimization: {
+      minimize: false
+    },
+    plugins: [
+      new CustomStatsPlugin()
+    ],
+    resolve: {
+      extensions: [
+        '.js',
+        '.json',
+        '.ts',
+      ],
+      mainFields: [
+        'main'
+      ],
+    },
+    target: 'node',
   }
-  @adobe/aio-lib-runtime:action-builder:debug   ]
-  @adobe/aio-lib-runtime:action-builder:debug }
-*/
+}
